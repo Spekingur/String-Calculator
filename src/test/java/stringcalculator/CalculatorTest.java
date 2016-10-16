7951461c2d2dfcb53279634731599442a70c3cc5
Hreiðar Ólafur Arnarsson <hreidara14@ru.is>
2016-10-16 23:22:40 +0000

    added tests for one number

--- src/test/java/stringcalculator/CalculatorTest.java
+++ src/test/java/stringcalculator/CalculatorTest.java
@@ -9,4 +9,11 @@ public class CalculatorTest {
 	public void testEmptyString() {
 		assertEquals(0, Calculator.add(""));
 	}
+
+	@Test
+	public void testOneNumber() {
+		assertEquals(1, Calculator.add("1"));
+		assertEquals(2, Calculator.add("2"));
+		assertEquals(3, Calculator.add("3"));
+	}
 }
