72668f479b9f0e31f177194b698fc065dff246e0
Hreiðar Ólafur Arnarsson <hreidara14@ru.is>
2016-10-16 23:43:04 +0000

    added code in Calculator to handle two numbers

--- src/main/java/stringcalculator/Calculator.java
+++ src/main/java/stringcalculator/Calculator.java
@@ -6,6 +6,10 @@ public class Calculator {
 		if (numbers.equals("")) {
 			return 0;
 		}
+		else if(numbers.contains(",")) {
+			String [] addNumbers = numbers.split(",");
+			return Integer.parseInt(addNumbers[0]) + Integer.parseInt(addNumbers[1]);
+		}
 		else {
 			return Integer.parseInt(numbers);
 		}
